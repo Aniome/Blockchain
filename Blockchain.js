de19dea8db30e231bdfa0c67ab0faf36d31a63e9
aniome <vaganov.kostya2000@yandex.ru>
2022-08-21 06:13:39 +0500

    add resolve conficts

--- Blockchain.js
+++ Blockchain.js
@@ -2,6 +2,7 @@ import { Block } from "./Block.js";
 import crypto from "node:crypto";
 import Url from "node:url";
 import QRCode from "qrcode";
+import fetch from "node-fetch";
 
 export class Blockchain {
 	constructor() {
@@ -33,7 +34,7 @@ export class Blockchain {
 		let parsed_url = Url.parse(address);
 		this.nodes.add(parsed_url);
 	}
-	valid_chain(chain) {
+	#valid_chain(chain) {
 		let prev_block = chain[0];
 		for (let i = 1; i < chain.length; i++) {
 			let block = chain[i];
@@ -47,12 +48,27 @@ export class Blockchain {
 		}
 		return true;
 	}
-	resolve_conflicts() {
+	async resolve_conflicts() {
 		let neighbours = this.nodes;
 		let new_chain = null;
 		let max_length = this.chain.length;
-		for (node of neighbours) {
+		for (let node of neighbours) {
+			const response = await fetch(`${node.href}chain`);
+			if (response.status == 200) {
+				const data = await response.json();
+				let length = data.length;
+				let chain = data.chain;
+				if (length > max_length && this.#valid_chain(chain)) {
+					max_length = length;
+					new_chain = chain;
+				}
+			}
 		}
+		if (new_chain) {
+			this.chain = new_chain;
+			return true;
+		}
+		return false;
 	}
 	generateQRcode(block) {
 		QRCode.toFile(`${block.index}.png`, JSON.stringify(block));
@@ -61,38 +77,3 @@ export class Blockchain {
 		return this.chain.at(-1);
 	}
 }
-
-/*
-    def resolve_conflicts(self):
-        """
-        Это алгоритм Консенсуса, он разрешает конфликты, 
-        заменяя цепь на самую длинную в цепи
-        True, если бы наша цепь была заменена, False, если нет.
-        """
-        neighbours = self.nodes
-        new_chain = None
-        # Ищем только цепи, длиннее нашей
-        max_length = len(self.chain)
-        # Захватываем и проверяем все цепи из всех узлов сети
-        for node in neighbours:
-            response = requests.get(f'http://{node}/chain')
-            if response.status_code == 200:
-                length = response.json()['length']
-                chain = response.json()['chain']
-                chain = self.in_class(chain)
-                # Проверяем, является ли длина самой длинной, а цепь - валидной
-                if length > max_length and self.valid_chain(chain):
-                    max_length = length
-                    new_chain = chain
-        # Заменяем нашу цепь, если найдем другую валидную и более длинную
-        if new_chain:
-            self.chain = new_chain
-            return True
-        return False
-
-    def generateQRcode(self, block):
-        file = f'QR/{block.index}.png'
-        block = block.in_dict()
-        img = qrcode.make(block)
-        img.save(file)
-*/
--- api.js
+++ api.js
@@ -23,50 +23,47 @@ app.post("/mine", (req, res) => {
 	res.status(200).json(block);
 });
 
-app.listen(PORT, () => console.log("server start"));
+app.get("/chain", (req, res) => {
+	let response = {
+		chain: blockchain.chain,
+		length: blockchain.chain.length,
+	};
+	res.status(200).json(response);
+});
+
+app.get("/nodes/register", (req, res) => {
+	const nodes = req.body.nodes;
+	for (let node of nodes) {
+		blockchain.register_node(node);
+	}
+	let response = {
+		message: "Новые узлы добавлены",
+		total_nodes: nodes,
+	};
+	res.status(201).json(response);
+});
+
+app.get("/nodes/resolve", (req, res) => {
+	let response;
+	blockchain.resolve_conflicts().then((ans) => {
+		if (ans) {
+			response = {
+				message: "Наша цепь была замена",
+				new_chain: blockchain.chain,
+			};
+		} else {
+			response = {
+				message: "Цепь находится в актуальном состоянии",
+				new_chain: blockchain.chain,
+			};
+		}
+		return res.status(200).json(response);
+	});
+});
+
+app.listen(PORT);
 
 /*
-@app.route('/chain', methods=['GET'])
-def full_chain():
-    response = {
-        'chain': blockchain.chain_in_dict(),
-        'length': len(blockchain.chain),
-    }
-    return jsonify(response), 200
-
-
-@app.route('/nodes/register', methods=['POST'])
-def register_nodes():
-    values = request.get_json()
-    nodes = values.get('nodes')
-    if nodes is None:
-        return "Error: Please supply a valid list of nodes", 400
-    for node in nodes:
-        blockchain.register_node(node)
-    response = {
-        'message': 'New nodes have been added',
-        'total_nodes': list(blockchain.nodes),
-    }
-    return jsonify(response), 201
-
-
-@app.route('/nodes/resolve', methods=['GET'])
-def consensus():
-    replaced = blockchain.resolve_conflicts()
-    chain = blockchain.chain_in_dict()
-    if replaced:
-        response = {
-            'message': 'Our chain was replaced',
-            'new_chain': chain
-        }
-    else:
-        response = {
-            'message': 'Our chain is authoritative',
-            'chain': chain
-        }
-    return jsonify(response), 200
-
-
 @app.route('/checkqr', methods=['POST'])
 def checkQR():
     data = request.get_json()
